8511a40e67356517de6b3eb74a2c461aa91f1016
Alex Caiedo <alex.caicedo@udla.edu.ec>
2023-12-31 09:29:16 -0500

    3er Commit: Creado NoteViewModel para manejar la lógica de NotePage. Actualizado NotePage.xaml para usar enlaces y comandos. Simplificado el código en NotePage.xaml.cs al cambiar a comandos.

--- Views/NotePage.xaml.cs
+++ Views/NotePage.xaml.cs
@@ -1,56 +1,9 @@
 namespace CaicedoRamos_TareaMVVM.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
     public NotePage()
 	{
         InitializeComponent();
-
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
-
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Nota note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.Nota note)
-        {
-            // Delete the file.
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private void LoadNote(string fileName)
-    {
-        Models.Nota noteModel = new Models.Nota();
-        noteModel.Filename = fileName;
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
-    }
-
-    public string ItemId
-    {
-        set { LoadNote(value); }
-    }
-
 }
